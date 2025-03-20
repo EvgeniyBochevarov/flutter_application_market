7c2914eb5392c10611f1a50de6d82f9892f563c3
EvgeniyBochevarov <bochevarov@gmail.com>
2025-03-20 18:20:36 +0200

    Фикс

--- lib/secondScreen.dart
+++ lib/secondScreen.dart
@@ -12,39 +12,50 @@ class SecondScreen extends StatelessWidget {
       ),
       body: Column(
         children: [
-          Center(
-            child: Text(
-              textAlign: TextAlign.center,
-              "Банні віники з карпатської ялиці – це прекрасний спосіб посилити ефект в лазні та отримати максимальну користь для здоров'я. Вони мають приємний аромат і м'які голочки, які не травмують шкіру.",
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                color: Colors.deepOrange,
+          Flexible(
+            child: Center(
+              child: Text(
+                textAlign: TextAlign.center,
+                "Банні віники з карпатської ялиці – це прекрасний спосіб посилити ефект в лазні та отримати максимальну користь для здоров'я. Вони мають приємний аромат і м'які голочки, які не травмують шкіру.",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.deepOrange,
+                ),
               ),
             ),
           ),
-          Container(
-            child: Image.asset('assets/jpg/pihtavenik.jpg', fit: BoxFit.cover),
+          Flexible(
+            child: Container(
+              child: Image.asset(
+                'assets/jpg/pihtavenik.jpg',
+                fit: BoxFit.contain,
+              ),
+            ),
           ),
-          Center(
-            child: Text(
-              textAlign: TextAlign.center,
-              "Тільки зараз ціна 100 грн.",
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                color: Colors.deepOrange,
+          Flexible(
+            child: Center(
+              child: Text(
+                textAlign: TextAlign.center,
+                "Тільки зараз ціна 100 грн.",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.deepOrange,
+                ),
               ),
             ),
           ),
 
-          SizedBox(height: 30),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.pushNamed(context, '/third');
-            },
+          //SizedBox(height: 10),
+          Flexible(
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/third');
+              },
 
-            child: Text('Товари'),
+              child: Text('Товари'),
+            ),
           ),
         ],
       ),
--- lib/thirdScreen.dart
+++ lib/thirdScreen.dart
@@ -5,30 +5,41 @@ class ThirdScreen extends StatelessWidget {
 
   Row buildRow(String path, String text1, String text2) {
     return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Flexible(
-          fit: FlexFit.tight,
-          child: Container(child: Image.asset(path, fit: BoxFit.cover)),
-        ),
         SizedBox(width: 10),
         Flexible(
           fit: FlexFit.tight,
           child: Container(
+            width: 150,
+            height: 120,
+            child: Image.asset(path, fit: BoxFit.contain),
+          ),
+        ),
+        Flexible(
+          fit: FlexFit.tight,
+          child: Container(
+            width: 150,
+            height: 120,
             padding: EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              border: Border.all(color: Colors.grey),
+            decoration: BoxDecoration(border: Border.all(color: Colors.grey)),
+            child: Align(child: Text(text1, textAlign: TextAlign.center)),
+          ),
+        ),
+        Flexible(
+          fit: FlexFit.loose,
+          child: Container(
+            width: 150,
+            height: 120,
+            padding: EdgeInsets.all(10),
+            decoration: BoxDecoration(border: Border.all(color: Colors.grey)),
+
+            child: Align(
+              child: Text(text2, style: TextStyle(fontWeight: FontWeight.bold)),
             ),
-            child: Text(text1),
           ),
         ),
         SizedBox(width: 10),
-        Flexible(
-          fit: FlexFit.tight,
-          child: Container(
-            child: Text(text2, style: TextStyle(fontWeight: FontWeight.bold)),
-          ),
-        ),
       ],
     );
   }
@@ -53,29 +64,51 @@ class ThirdScreen extends StatelessWidget {
             ),
           ),
           SizedBox(height: 30),
-          buildRow(
-            'assets/jpg/med.jpg',
-            'Мед для лазні "Квітковий"',
-            '100 грн',
+          Flexible(
+            child: buildRow(
+              'assets/jpg/med.jpg',
+              'Мед для лазні "Квітковий"',
+              '100 грн',
+            ),
           ),
           SizedBox(height: 10),
-          buildRow(
-            'assets/jpg/kovsh.jpg',
-            "Ківш для лазні з дерев'яною ручкою",
-            '465 грн',
+          Flexible(
+            child: buildRow(
+              'assets/jpg/kovsh.jpg',
+              "Ківш для лазні з дерев'яною ручкою",
+              '465 грн',
+            ),
           ),
           SizedBox(height: 10),
-          buildRow(
-            'assets/jpg/shapka.jpg',
-            "Шапка для бані «Патріотична», в’язана",
-            '700 грн',
+          Flexible(
+            child: buildRow(
+              'assets/jpg/shapka.jpg',
+              "Шапка для бані «Патріотична», в’язана",
+              '700 грн',
+            ),
           ),
+          SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Flexible(
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text('Назад'),
+                ),
+              ),
 
-          ElevatedButton(
-            onPressed: () {
-              Navigator.pushNamed(context, '/');
-            },
-            child: Text('Головна'),
+              Flexible(
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/');
+                  },
+                  child: Text('Головна'),
+                ),
+              ),
+            ],
           ),
         ],
       ),
